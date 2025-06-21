e5aefe3566c10a0d4ebc91d9dccb05e784bc5f55
canalmar <marisatcanale@gmail.com>
2025-06-21 09:42:45 -0300

    feat: user profile edit, phone/address on signup, navbar with profile and about

--- client/apps.py
+++ client/apps.py
@@ -6,6 +6,6 @@ class ClientConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "client"
 
-    def ready(self):
-        # Importa aquí para que las señales se enganchen al arrancar Django
-        import client.signals  # noqa
+#    def ready(self):
+#        # Importa aquí para que las señales se enganchen al arrancar Django
+#        import client.signals  # noqa
